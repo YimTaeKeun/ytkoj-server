73e6831d4fafee8ae99dd34a9cd06073f67a0949
YimTaeKeun <ytk030305@gmail.com>
2026-05-05 23:40:25 +0900

    INIT: Project Init

---
+++ src/main/java/com/ytk/ytkoj/YtkojApplication.java
@@ -0,0 +1,13 @@
+package com.ytk.ytkoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtkojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtkojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ytk/ytkoj/YtkojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytk.ytkoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtkojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
