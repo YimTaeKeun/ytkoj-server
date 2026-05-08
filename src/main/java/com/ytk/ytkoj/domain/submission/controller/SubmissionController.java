7bf5c1ed054ca8ffa4ba41b20a4b7f01c1b91501
YimTaeKeun <ytk030305@gmail.com>
2026-05-09 00:00:04 +0900

    FEAT: 채점 컨트롤러 개발

--- src/main/java/com/ytk/ytkoj/domain/submission/controller/SubmissionController.java
+++ src/main/java/com/ytk/ytkoj/domain/submission/controller/SubmissionController.java
@@ -1,16 +1,27 @@
 package com.ytk.ytkoj.domain.submission.controller;
 
 import com.ytk.ytkoj.domain.submission.dto.RequestDTOs;
+import com.ytk.ytkoj.domain.submission.service.SubmissionService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/submissions")
+@RequiredArgsConstructor
 public class SubmissionController {
+    private final SubmissionService submissionService;
+
 
     @PostMapping("")
     public ResponseEntity<?> submit(@RequestBody RequestDTOs.SubmissionRequest request){
-        return ResponseEntity.noContent().build();
+        submissionService.gradingCode(
+                request.problemId(),
+                request.lang(),
+                request.sourceCode()
+        );
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("")
@@ -19,5 +30,18 @@ public class SubmissionController {
             @RequestParam(required = false) String problemId
     ){
         return ResponseEntity.noContent().build();
+    }
+
+    @Operation(
+            summary = "채점 결과 회부",
+            description = "셀러리에서 채점한 결과를 다시 받아내는 오퍼레이션입니다."
+    )
+    @PostMapping("/result")
+    public void getSubmissionResult(@RequestBody RequestDTOs.GradingResult result){
+        submissionService.updateSubmissionStatus(
+                result.taskId(),
+                result.status(),
+                result.message()
+        );
     }
 }
